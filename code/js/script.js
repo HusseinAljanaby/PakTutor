02f938e23052ce4f4a7761c95d254dbcba52f924
Hussein Aljanaby <husseinalaa.aljanaby@gmail.com>
2016-02-25 19:16:44 +1300

    update

--- code/js/script.js
+++ code/js/script.js
@@ -1,9 +1,11 @@
 var ref = new Firebase("https://paktutor.firebaseio.com");
 
-ref.authWithOAuthRedirect("google", function(error) {
-  if (error) {
-    console.log("Authentication Failed!", error);
-  } else {
-    // We'll never get here, as the page will redirect on success.
-  }
-});
+function loginGoogle() {
+	ref.authWithOAuthRedirect("google", function(error) {
+	if (error) {
+		console.log("Authentication Failed!", error);
+	} else {
+		// We'll never get here, as the page will redirect on success.
+		}
+	});
+}
